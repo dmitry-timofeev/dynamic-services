bb011a2c0629b3faa1aa99304117457b834c63d9
Dmitry Timofeev <dmytro.tymofeev@xdev.re>
2019-02-11 16:53:40 +0200

    Make DynamicModulesMain load plugins dynamically
    Load the plugins paths to which are specified on the command
    line.

--- service-runtime/src/main/java/com/exonum/osgi/prototype/DynamicModulesMain.java
+++ service-runtime/src/main/java/com/exonum/osgi/prototype/DynamicModulesMain.java
@@ -1,50 +1,77 @@
 package com.exonum.osgi.prototype;
 
-import com.exonum.osgi.prototype.service.UserService;
-import java.io.Console;
+import static java.util.stream.Collectors.toList;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.pf4j.DefaultPluginManager;
+import org.pf4j.PluginManager;
 
 public class DynamicModulesMain {
 
+  // /home/dmitry/Documents/dynamic-services-makarov-proto/service-address/target/service-address-1.0-SNAPSHOT-plugin.jar /home/dmitry/Documents/dynamic-services-makarov-proto/service-user/target/service-user-1.0-SNAPSHOT-plugin.jar
   public static void main(String[] args) {
-    String basePath;
-    if (args.length != 1) {
-      basePath = "";
-    } else {
-      basePath = args[0];
-    }
+    List<Path> pluginPaths = Arrays.stream(args)
+        .map(path -> Path.of(path))
+        .collect(toList());
 
-    UserService userService = null;
+    PluginManager pluginManager = new DefaultPluginManager();
 
-    boolean active = true;
-    while (active) {
-      Console console = System.console();
-      String command = console.readLine("command: ");
+    List<String> loadedPlugins = loadPlugins(pluginManager, pluginPaths);
 
-      String moduleName;
-      switch (command) {
-        case "load":
-          moduleName = console.readLine("module name: ");
-          System.out.println("Loading module: " + moduleName);
-          //loader.loadModule(moduleName, null);
-          continue;
-        case "unload":
-          moduleName = console.readLine("module name: ");
-          System.out.println("UnLoading module: " + moduleName);
-          continue;
-        case "exec":
-          String userName = console.readLine("user name: ");
-          System.out.println("Executing logic.");
-          userService.getUserDetails(userName);
-          continue;
-        case "exit":
-          System.out.println("Exiting application");
-          active = false;
-          continue;
-        default:
-          System.out.println("Unknown command. Use load, unload, exit commands");
+    startPlugins(pluginManager, loadedPlugins);
+
+    stopPlugins(pluginManager, loadedPlugins);
+
+    unloadPlugins(pluginManager, loadedPlugins);
+  }
+
+  /**
+   * Loads the plugins by paths to their JAR-files.
+   * @param pluginJars a list of plugins to load
+   * @return a list of pluginIds of loaded plugins
+   * @throws IllegalArgumentException if some plugins could not be loaded
+   */
+  private static List<String> loadPlugins(PluginManager pluginManager, List<Path> pluginJars) {
+    List<String> loadedPlugins = new ArrayList<>(pluginJars.size());
+    for (Path pluginJar : pluginJars) {
+      System.out.println("\uD83D\uDE80 Loading " + pluginJar);
+
+      String pluginId = pluginManager.loadPlugin(pluginJar);
+      if (pluginId == null) {
+        // Why don't PluginManager#loadPlugin throw itself? How could clients possibly know
+        // why it failed to load a plugin?
+        throw new IllegalArgumentException("Could not load plugin " + pluginJar);
       }
-    }
 
+      System.out.println("\uD83D\uDE80 Loaded " + pluginId + " (" + pluginJar + ")");
+      loadedPlugins.add(pluginId);
+    }
+    return loadedPlugins;
+  }
+
+  private static void startPlugins(PluginManager pluginManager, List<String> plugins) {
+    System.out.println("Starting plugins: " + plugins);
+
+    plugins.forEach(pluginManager::startPlugin);
+
+    System.out.println("Started plugins: " + pluginManager.getStartedPlugins());
+  }
+
+  private static void stopPlugins(PluginManager pluginManager, List<String> plugins) {
+    System.out.println("Stopping plugins " + plugins);
+
+    plugins.stream()
+        .peek(pluginId -> System.out.println("Stopping " + pluginId))
+        .forEach(pluginManager::stopPlugin);
+  }
+
+  private static void unloadPlugins(PluginManager pluginManager, List<String> loadedPlugins) {
+    System.out.println("Unloading plugins " + loadedPlugins);
+
+    loadedPlugins.forEach(pluginManager::unloadPlugin);
   }
 
 }
